4c8f275f3e8d417abed9c7eeb07bf13d02b4cf3d
Hamid Habibi <68816201+MrHamid1999@users.noreply.github.com>
2022-08-24 16:13:17 +0430

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+   <html lang="en" dir="ltr">
+
+   <head>
+     <meta charset="utf-8">
+     <title>Ben Nguyen's Website</title>
+   </head>
+
+   <body>
+     <button type="button" id="btn-1" name="button">right</button>
+     <button type="button" id="btn-1" name="button">left</button>
+</body>
+
+<script type="text/javascript" src="index.js">
+
+</script>
+
+</html>
